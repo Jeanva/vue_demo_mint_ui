a69f3ed45878db2479bd0ace9b9ef4905c30b38b
Jeanva <591975433@qq.com>
2018-03-22 19:02:21 +0800

    demo42

---
+++ demo42/src/components/HeaderView.vue
@@ -0,0 +1,14 @@
+<template>
+
+	<div>
+		<mt-header title='标题过长会隐藏后面的内容哈哈哈哈哈哈哈哈'>
+
+		
+			<mt-button icon='back' slot='left'>返回</mt-button>
+		
+		
+		<mt-button icon='more' slot='right'>收藏</mt-button>
+		</mt-header>
+	</div>
+
+</template>
